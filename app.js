be320faf6728617a1b4aadc7c9132d2243790818
Supakit <supakit.pnm@gmail.com>
2022-07-17 21:57:54 +0700

    Using Express Router

--- app.js
+++ app.js
@@ -5,22 +5,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use(adminRoutes);
 
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
-});
+app.use(shopRoute);
 
 const server = http.createServer(app);
 
