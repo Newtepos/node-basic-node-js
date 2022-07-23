f09843d318fd8afd045262c28d35e3042fc6b367
Supakit <supakit.pnm@gmail.com>
2022-07-23 15:11:41 +0700

    express-handlerbar template

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const handlerbars = require("express-handlebars");
 
 const app = express();
+app.engine(
+  "hbs",
+  handlerbars({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
 
-app.set("view engine", "pug");
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -18,8 +27,8 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found'});
-//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  //   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
