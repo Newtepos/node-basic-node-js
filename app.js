3413b20d52c14dab45c57596a6e7c6ea76c1fb55
Supakit <supakit.pnm@gmail.com>
2022-07-24 16:13:23 +0700

    Add Erorr Controller

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require('./controller/error')
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,9 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: '/' });
-  //   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
+app.use(errorController.get404Page);
 
 app.listen(3000);
