76f1efb371cc920dc940829a0a83fa605d549112
Supakit <supakit.pnm@gmail.com>
2022-07-26 21:43:37 +0700

    Refactor view and controller

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,17 @@
-const express = require("express");
+const path = require('path');
 
-const productController = require('../controller/product')
+const express = require('express');
+
+const shopController = require('../controller/shop.js');
 
 const router = express.Router();
 
-router.get("/", productController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
