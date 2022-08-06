72379b817a34ded64d2ea5bb90ea98b7d334f910
Supakit <supakit.pnm@gmail.com>
2022-08-06 14:13:53 +0700

    Add: Display Edit Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editMode: false
   });
 };
 
@@ -17,15 +15,28 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
+  res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  const productId = req.params.productId;
+  Product.findProductbyId(productId, (product) => {
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/Edit-product",
+      editMode,
+      product,
     });
   });
 };
