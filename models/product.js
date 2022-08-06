69b9bb246b4640fcfaed7f93edcf3b3da20af36b
Supakit <supakit.pnm@gmail.com>
2022-08-06 14:41:43 +0700

    Add: Edit Product Data

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const fs = require("fs");
 const path = require("path");
 
@@ -18,7 +19,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +28,21 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if(this.id) {
+        const exisitngProductIndex = products.findIndex(prod => prod.id === this.id);
+        const existingProducts = [...products];
+        existingProducts[exisitngProductIndex] = this
+        fs.writeFile(p, JSON.stringify(existingProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -42,7 +53,7 @@ module.exports = class Product {
   static findProductbyId(id, cb) {
     getProductsFromFile((products) => {
       const product = products.find((p) => p.id == id);
-      cb(product)
+      cb(product);
     });
   }
 };
